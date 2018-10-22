13a2b151f7b45d3af9b49afbf85c14c9d003b069
Lisa Sampson <lisasampson0918@gmail.com>
2018-10-22 16:36:34 -0400

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Lisa Sampson on 10/22/18.
+//  Copyright © 2018 Lisa M Sampson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
